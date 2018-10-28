e228c6e184a0be7b73f79e99b1dc515ee9c9d21b
Thomas Schur <cyb10101@gmail.com>
2018-10-28 23:46:00 +0000

    [TASK] Throw exception if user has not included any typoscript

--- Classes/Utility/ConfigurationUtility.php
+++ Classes/Utility/ConfigurationUtility.php
@@ -57,6 +57,9 @@ class ConfigurationUtility
             }
             /** @var TypoScriptService $typoScriptService */
             $typoScriptService = $objectManager->get(TypoScriptService::class);
+            if (empty($fullTypoScript['module.']['tx_maillogger.'])) {
+                throw new \Exception('Constants and setup TypoScript are not included!');
+            }
             self::$currentModuleConfiguration = $typoScriptService->convertTypoScriptArrayToPlainArray($fullTypoScript['module.']['tx_maillogger.']);
         }
         return self::$currentModuleConfiguration[$key];
