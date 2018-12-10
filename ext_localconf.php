dfcbd7f3fec01e04eb2767f12d9771f96ddfeade
Cyb10101 <cyb10101@gmail.com>
2018-12-10 19:16:37 +0100

    [BUGFIX] Module TypoScript should be loaded global

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,13 @@
 <?php
 
-
 // XClass TYPO3 Mail Objects
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Mail\MailMessage::class] = [
     'className' => \Pluswerk\MailLogger\Domain\Model\LoggableMailMessage::class,
 ];
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mail_logger/Configuration/TypoScript/constants.typoscript">'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mail_logger/Configuration/TypoScript/setup.typoscript">'
+);
