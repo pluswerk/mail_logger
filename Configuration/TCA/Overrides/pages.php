d2a9e8c6853f57ebc36e6bf4f2c27dcbc86f8299
Markus Hölzle <mail@markus-hoelzle.de>
2021-02-05 11:38:41 +0100

    Restructure configuration files to follow TYPO3 rules
    See details in the TYPO3 documentation:
    https://docs.typo3.org/m/typo3/reference-coreapi/master/en-us/ExtensionArchitecture/ConfigurationFiles/Index.html

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,7 @@
+<?php
+defined('TYPO3') or die();
+
+(function () {
+    // Add container for mail templates
+    Pluswerk\MailLogger\Utility\BackendConfigurationUtility::registerContainerFolders($GLOBALS['TCA']['pages']);
+})();
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,9 @@
 <?php
+defined('TYPO3') or die();
 
-// XClass TYPO3 Mail Objects
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Mail\MailMessage::class] = [
-    'className' => \Pluswerk\MailLogger\Domain\Model\LoggableMailMessage::class,
-];
+(function () {
+    // XClass TYPO3 Mail Objects
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Mail\MailMessage::class] = [
+        'className' => \Pluswerk\MailLogger\Domain\Model\LoggableMailMessage::class,
+    ];
+})();
